5a8956e9ecc20c1c75b318daa3b65336dbde6c4a
Diogoafg7 <diogoafg7@gmail.com>
2024-01-02 00:54:02 +0000

    Tests refactor

--- src/Tests.hs
+++ src/Tests.hs
@@ -11,6 +11,7 @@ testAssembler code = (stack2Str stack, state2Str state)
 
 runAllAssemblerTests :: IO ()
 runAllAssemblerTests = do
+    putStrLn "Assembler Tests:"
     print $ testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
     print $ testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
     print $ testAssembler [Fals,Store "var",Fetch "var"]  == ("False","var=False")
@@ -26,6 +27,8 @@ runAllAssemblerTests = do
 -- If you test:
     --print $ testAssembler[Tru,Tru,Store "y", Fetch "x",Tru]
 -- You should get an exception with the string: "Run-time error"
+    putStrLn "All tests passed."
+    putStrLn ""
 
 
 testParser :: String -> (String, String)
@@ -35,15 +38,18 @@ testParser programCode = (stack2Str stack, state2Str state)
 
 runAllParserTests :: IO ()
 runAllParserTests = do
- print $ testParser "x := 5; x := x - 1;" == ("","x=4")
- print $ testParser "x := 0 - 2;" == ("","x=-2")
- print $ testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
- print $ testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
- print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
- print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
- print $ testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
- print $ testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
- print $ testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
- print $ testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
- print $ testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
- print $ testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+  putStrLn "Parser Tests:"
+  print $ testParser "x := 5; x := x - 1;" == ("", "x=4")
+  print $ testParser "x := 0 - 2;" == ("", "x=-2")
+  print $ testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("", "y=2")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("", "x=1")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("", "x=2")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("", "x=2,z=4")
+  print $ testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("", "x=34,y=68")
+  print $ testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("", "x=34")
+  print $ testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("", "x=1")
+  print $ testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("", "x=2")
+  print $ testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("", "x=2,y=-10,z=6")
+  print $ testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("", "fact=3628800,i=1")
+  putStrLn "All tests passed."
+  putStrLn ""
